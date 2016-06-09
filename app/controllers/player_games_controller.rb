a6bfd872ad8465e7d1f2f8587eea7b5d6e0aec7d
Jeremie Carlson <findme@jeremiecarlson.com>
2016-06-09 11:51:28 -0700

    get played games to have a nice json output

--- app/controllers/player_games_controller.rb
+++ app/controllers/player_games_controller.rb
@@ -6,5 +6,17 @@ class PlayerGamesController < ApplicationController
                       .where(player_id: @player.id)
                       .flat_map(&:history_events)
                       .map { |event| [event.created_at, event.value] }
+
+    respond_to do |format|
+      format.html
+      format.json do
+        # render json: @game
+        render json: [
+          { player: {id: @player.id, name: @player.name, email: @player.email} },
+          { game: @game },
+          { rating_history: @chart_data }
+        ]
+      end
+    end
   end
 end
